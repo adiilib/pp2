dfa630e92364717ff97ddcfd1cfa3f618c8bc91a
adiilib <adilibi07@gmail.com>
2026-02-01 23:16:21 +0500

    18

---
+++ lab2/task18.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = [input().strip() for _ in range(n)]
+
+first_index = {}
+for i, s in enumerate(arr):
+    if s not in first_index:
+        first_index[s] = i + 1  
+for s in sorted(first_index):
+    print(s, first_index[s])
