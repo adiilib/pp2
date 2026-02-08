b30679c205d40e74bebb105e141616d9deececda
adiilib <adilibi07@gmail.com>
2026-02-08 15:24:54 +0500

    8

--- lab3/task8.py
+++ lab3/task8.py
@@ -1,15 +1,23 @@
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+class Account:
+    def __init__(self, balance):
+        self.balance = balance
 
-    def area(self):
-        pi = 3.14159
-        return pi * self.radius ** 2
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient Funds"
+        self.balance -= amount
+        return self.balance
 
 
-r = int(input())
 
-c = Circle(r)
+balance, withdraw_amount = map(int, input().split())
 
 
-print(f"{c.area():.2f}")
+acc = Account(balance)
+
+
+result = acc.withdraw(withdraw_amount)
+print(result)
