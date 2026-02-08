22a38f45fd2017bf025518a8291af39204e88cd6
adiilib <adilibi07@gmail.com>
2026-02-08 15:32:56 +0500

    13

---
+++ lab3/task13.py
@@ -0,0 +1,7 @@
+nums = list(map(int, input().split()))
+
+is_prime = lambda x: x > 1 and all(x % i != 0 for i in range(2, int(x**0.5)+1))
+
+primes = list(filter(is_prime, nums))
+
+print(*primes if primes else ["No primes"])
