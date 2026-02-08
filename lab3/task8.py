9d4f9e620916a2a1db599dd0fac3ea6e37309f31
adiilib <adilibi07@gmail.com>
2026-02-08 14:57:48 +0500

    8

---
+++ lab3/task8.py
@@ -0,0 +1,20 @@
+class Account:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient Funds"
+        self.balance -= amount
+        return self.balance
+
+
+balance, withdraw_amount = map(int, input().split())
+
+acc = Account(balance)
+
+result = acc.withdraw(withdraw_amount)
+print(result)
